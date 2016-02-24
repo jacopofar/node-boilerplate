20555e522267ab343ef0ea2f5f3a2021f9ffe2bc
jacopo_farina <jacopo.farina@spindox.it>
2016-02-24 14:44:57 +0100

    added websocket on both client and server side

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-io.on('connection', function(){ /* … */ });
+const path = require('path');
+app.use(express.static(path.join(__dirname,'static')));
+io.on('connection', (socket) => {
+  console.log("new connection from a websocket");
+  setInterval(function(){
+    socket.emit('timestamp',new Date().toISOString());
+  },900);
+});
+
 //TODO don't user an hardcoded port
 server.listen(3000);
