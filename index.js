a10a00ad779021898a7c5265eb34f2e51d339a1c
jacopo_farina <jacopo.farina@spindox.it>
2016-03-06 17:06:44 +0100

    added SQLite

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const path = require('path');
 const nconf = require('nconf');
+const sqlite3 = require('sqlite3').verbose();
+
 nconf.formats.yaml = require('nconf-yaml');
 
 global.conf = nconf.argv()
    .env()
    .file({ file: path.join(__dirname,'config.yaml'), format: nconf.formats.yaml });
+
+global.db = new sqlite3.Database(conf.get('db_path'));
 
 app.use(express.static(path.join(__dirname,'static')));
 io.on('connection', (socket) => {
@@ -22,7 +26,6 @@ io.on('connection', (socket) => {
       // we tell the client to execute 'new message'
       socket.broadcast.emit('update text', data);
     });
-
 });
 
 const port = conf.get('port');
